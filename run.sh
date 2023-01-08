0a45ac855aef3ecabff73482582a8529cdaeffef
dungngminh <ngminhdung1311@gmail.com>
2023-01-09 00:46:30 +0700

    chore: add run scripts

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Author : dungngminh
+# Script follows here:
+
+# run `flutter pub get`
+echo "Run flutter pub get:"
+flutter pub get
+
+# run `build runner`
+echo "Run build_runner:"
+flutter pub run build_runner build --delete-conflicting-outputs
+
