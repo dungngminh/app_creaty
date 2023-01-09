4709151fcc0f5c74a95ac91ef32eb2b7a156267e
dungngminh <ngminhdung1311@gmail.com>
2023-01-10 00:40:25 +0700

    feature(home_screen): add preview device

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,84 @@
+import 'package:device_frame/device_frame.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late DeviceInfo _deviceInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _deviceInfo = Devices.ios.iPad;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: DeviceFrame(
+              device: _deviceInfo,
+              screen: const AppDevice(),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.change_circle,
+        ),
+        onPressed: () {
+          setState(() {
+            _deviceInfo = Devices.android.samsungGalaxyA50;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class AppDevice extends StatelessWidget {
+  const AppDevice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Creaty',
+      debugShowCheckedModeBanner: false,
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.blue,
+        useMaterial3: true,
+        useMaterial3ErrorColors: true,
+      ),
+      darkTheme: FlexThemeData.light(
+        scheme: FlexScheme.blue,
+        useMaterial3: true,
+        useMaterial3ErrorColors: true,
+      ),
+      home: const HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('hehe'),
+      ),
+    );
   }
 }
